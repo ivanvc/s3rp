b096789d7992c380eb6dfd0a20831368de4b780b
Ivan Valdes (@ivanvc) <iv@a.ki>
2019-12-21 15:57:00 -0800

    Add port to config, move flag.Parse

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"flag"
 	"os"
+	"strings"
 
 	"github.com/ivanvc/s3-reverse-proxy/pkg/log"
 )
@@ -11,16 +12,15 @@ var (
 	indexPage = flag.String("index", "index.html", "index page (default: index.html)")
 	bucket    = flag.String("bucket", "", "bucket name")
 	host      = flag.String("host", ":8080", "host to listen on (default: \":8080\")")
+	port      = flag.String("port", "", "port to listen on, overrides host's")
 	IndexPage string
 	Bucket    string
 	Host      string
 )
 
-func init() {
-	flag.Parse()
-}
-
 func Load() {
+	flag.Parse()
+
 	if v := os.Getenv("BUCKET"); len(v) > 0 {
 		Bucket = v
 	} else {
@@ -30,6 +30,13 @@ func Load() {
 		Host = v
 	} else {
 		Host = *host
+	}
+	if v := os.Getenv("PORT"); len(v) > 0 {
+		port = &v
+	}
+	if len(*port) > 0 {
+		Host = strings.TrimRightFunc(Host, func(r rune) bool { return r != ':' })
+		Host += *port
 	}
 	if v := os.Getenv("INDEX_PAGE"); len(v) > 0 {
 		IndexPage = v
