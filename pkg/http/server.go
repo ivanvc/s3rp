2fa938fd790353dc01cfb8c1d3fca5d2f847c9c5
Ivan Valdes (@ivanvc) <iv@a.ki>
2019-12-21 16:19:13 -0800

    Add /healthz

--- pkg/http/server.go
+++ pkg/http/server.go
@@ -11,6 +11,11 @@ import (
 
 // Serve starts the HTTP server.
 func Serve(sess *session.Session) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+	mux.Handle("/", newProxy(sess))
 	log.Logger.Println("Ready, listening on", config.Host)
-	log.Logger.Fatal(http.ListenAndServe(config.Host, newProxy(sess)))
+	log.Logger.Fatal(http.ListenAndServe(config.Host, mux))
 }
